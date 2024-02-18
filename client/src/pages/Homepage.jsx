d40f86519105f672780b9ea1ef54cba9938d0854
esmael <esmaelabdlkadr00@gmail.com>
2024-02-18 10:52:16 +0300

    category  completed

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,9 +1,14 @@
+import Ctegories from "../components/Ctegories"
 import Header from "../components/Header"
+import Hero from "../components/Hero"
+import styles from "../styles/styles"
 
 function Homepage() {
     return (
-        <div>
-           <Header/> 
+        <div >
+           <Header/>
+           <Hero/> 
+           <Ctegories/>
         </div>
     )
 }
