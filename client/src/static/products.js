b28365aa8e92f5eddce5ee0422ffc49688840070
esmael <esmaelabdlkadr00@gmail.com>
2024-02-19 11:07:51 +0300

    productd modal  completed

--- client/src/static/products.js
+++ client/src/static/products.js
@@ -1,41 +1,81 @@
 import i1 from "../assets/product/card2.jpg"
+import shop from "../assets/product/slide4.jpg"
 
 const products=[
     {
     name:"mango",
-     description: 'This is the description of the first item.',
+     description: 'This is the description of the first item.This is the description of the first item.This is the description of the first item.This is the description of the first item.This is the description of the first item.This is the description of the first item.This is the description of the first item.This is the description of the first item.This is the description of the first item.This is the description of the first item.This is the description of the first item.This is the description of the first item.This is the description of the first item.This is the description of the first item.This is the description of the first item.This is the description of the first item.This is the description of the first item.This is the description of the first item.This is the description of the first item.This is the description of the first item.This is the description of the first item.',
     img:i1,
-    value:30,},
+    value:30,
+    price:500,
+    shopName:"shemsu",
+    shopRating:4.5,
+    totalSell:30,
+    priceDiscount:200,
+    shopAvatar:shop
+
+},
     {
     name:"avocado",
      description: 'This is the description of the first item.',
      img:i1,
-    value:83,},
+    value:83,
+    price:500,
+    shopName:"shemsu",
+    shopRating:4.5,
+totalSell:30,
+    shopAvatar:shop
+},
     {
     name:"banana",
      description: 'This is the description of the first item.',
      img:i1,
-    value:73,},
+    value:73,
+    price:500,
+    shopName:"shemsu",
+    shopRating:4.5,
+totalSell:30,},
     {
     name:"juicy",
      description: 'This is the description of the first item.',
      img:i1,
-    value:3,},
+    value:3,
+    price:500,
+    shopName:"shemsu",
+    shopRating:4.5,
+   totalSell:30,
+       shopAvatar:shop
+    
+},
     {
     name:"papaya",
      description: 'This is the description of the first item.',
      img:i1,
-    value:3,},
+    value:3,
+price:500,
+shopName:"shemsu",
+shopRating:4.5,
+totalSell:30
+},
+    
     {
     name:"runn",
      description: 'This is the description of the first item.',
      img:i1,
-    value:38,},
+    value:38,
+    shopName:"shemsu",
+    shopRating:4.5,
+},
     {
     name:"mango",
      description: 'This is the description of the first item.',
      img:i1,
-    value:3,},
+    value:3,
+    totalSell:20,
+    shopName:"shemsu",
+    shopRating:4.5,
+}
+    ,
 
 ];
 export default products
