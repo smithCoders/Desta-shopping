2acac640ecb4a9e3d6bfe5a4a809ea9dcc072b71
esmael <esmaelabdlkadr00@gmail.com>
2024-02-22 11:06:47 +0300

    re developing UI- remove trash

--- client/src/static/navItems.js
+++
@@ -1,8 +0,0 @@
-const navItems=[
-    {name:"Home",url:"/"},
-    {name:"Best selling",url:"/best-selling"},
-    {name:"Products",url:"/products"},
-    {name:"Events",url:"/events"},
-    {name:"FAQ",url:"/faq"},
-];
-export default navItems
